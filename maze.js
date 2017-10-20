fbbb2258a9fd0d16b83c4a1179d422b090347c86
daniadobbs <dania.dobbs@gmail.com>
2017-10-20 01:22:32 -0500

    single boundary to turn red

--- maze.js
+++ maze.js
@@ -1,45 +1,13 @@
 "use strict";
+var winner=true; //keeps track of boundary hits
+var start= document.getElementById("start");
+var end=document.getElementById("end");
 
-var start= $$('#start');
-var end= $$('#end');
-var winner=true;//keeps track of whether or not the mouse hits a boundary
-var status= $$('#status'); //changes status if player wins/loses/restarts
-var boundaries=document.querySelectorAll(".boundary")//selects all boundaries
-
-window.onload=function(){
-    start.onclick= gameStart;
-    end.onmouseover= gameOver;
-
+window.onload= function(){
+  $("boundary1").onmouseover= turnRed;
 }
-
-function Loser(){
-  if(winner){
-    status.textContent="Winner!";
-  }
-  else{
-      status.textContent="You Lose! Better Luck Next Time."
-  }
-}
-
-
-function gameStart(){
-  winner=true;
-  status.textContent="Make it to the end. Good Luck!"
-  for (var i = 0; i < boundary.length; i++) {
-        boundary[i].removeClassName("youlose");
-    }
-
-}
-
 
 function turnRed(){
-  winner=false;
-
-  for(var i=0; i= boundaries.length-1;i++){
-    boundaries[i].addClassName= "youlose";
-  }
+  $("boundary1").addClassName("youlose"); //adds youlose CSS property (turns boundary red if touched by mouse)
 }
 
-function gameOver(){
-
-}
